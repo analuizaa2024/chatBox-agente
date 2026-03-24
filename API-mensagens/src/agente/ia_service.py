6e4fe150012e0328bef86df76fdd2a22423d3edd
analuizaa2024 <analuizapequenotal@gmail.com>
2026-03-23 21:26:20 -0300

    troquei a IA

--- API-mensagens/src/agente/ia_service.py
+++ API-mensagens/src/agente/ia_service.py
@@ -1,30 +1,28 @@
-import os
 import requests
 
-API_KEY = os.getenv("API_KEY")
-
-API_URL = "https://router.huggingface.co/hf-inference/models/gpt2"
-
-headers = {
-    "Authorization": f"Bearer {API_KEY}"
-}
+API_KEY = "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"
 
 def gerar_resposta(pergunta):
 
-    payload = {
-        "inputs": pergunta,
-        "parameters": {
-            "max_new_tokens": 100
-        }
+    url = "https://openrouter.ai/api/v1/chat/completions"
+
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
     }
 
-    response = requests.post(API_URL, headers=headers, json=payload)
+    payload = {
+        "model": "mistralai/mistral-7b-instruct",
+        "messages": [
+            {
+                "role": "user",
+                "content": pergunta
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
 
     data = response.json()
 
-    print(data)  # <-- para ver o que a API está retornando
-
-    if isinstance(data, list):
-        return data[0]["generated_text"]
-    else:
-        return "Erro ao gerar resposta da IA"
+    return data["choices"][0]["message"]["content"]
