1d42b27df95a8bca86709fe63124c6995a5bd58c
analuiza2024 <analuizapequenotal@gmail.com>
2026-07-14 17:43:50 -0300

    fiz o userSlice

--- my-react-app/src/redux/userSlice.js
+++ my-react-app/src/redux/userSlice.js
@@ -1,9 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
-    name: "user",
+const userSlice = createSlice({
+     name: "user",
 
     initialState: {
         user: null
+    },
+
+    reducers: {
+        login(state, action) {
+            state.user = action.payload;
+        },
+
+        logout(state) {
+            state.user = null;
+
+        },
+
+        editarPerfil(state, action) {
+            state.user = {
+                ...state.user,
+                ...action.payload
+            }
+
+
+        }
+
+        
     }
+
+   
 })
+
+export default  userSlice.reducer;
+export const {login, logout, editarPerfil} = userSlice.actions;
+
